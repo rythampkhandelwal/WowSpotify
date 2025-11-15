118e634adb884f86c1725a349641df9b679b0783
Rytham Khandelwal <rythampkhandelwal@gmail.com>
2025-11-15 18:02:29 +0530

    Update Install_Auto.bat

--- scripts/Install_Auto.bat
+++ scripts/Install_Auto.bat
@@ -1,10 +1,10 @@
 @echo off
 
-:: Line for changing spotx parameters, each parameter should be separated by a space
+:: Line for changing WowSpotify parameters, each parameter should be separated by a space
 set param=-confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify
 
-set url='https://raw.githubusercontent.com/rythampkhandelwal/SpotX/refs/heads/main/run.ps1'
-set url2='https://raw.githubusercontent.com/rythampkhandelwal/SpotX/refs/heads/main/run.ps1'
+set url='https://raw.githubusercontent.com/rythampkhandelwal/WowSpotify/refs/heads/master/run.ps1'
+set url2='https://raw.githubusercontent.com/rythampkhandelwal/WowSpotify/refs/heads/master/run.ps1'
 set tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
 
 %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe ^
