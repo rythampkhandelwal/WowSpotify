19f2bb59bc0e2f20088e985c44b996d8f04a9a27
Rytham Khandelwal <rythampkhandelwal@gmail.com>
2025-11-15 18:01:38 +0530

    Update Install_Old_theme.bat

--- Install_Old_theme.bat
+++ Install_Old_theme.bat
@@ -3,8 +3,8 @@
 :: Line for changing WowSpotify parameters, each parameter should be separated by a space
 set param=-v 1.2.13.661.ga588f749-4064 -confirm_spoti_recomended_over -block_update_on
 
-set url='https://raw.githubusercontent.com/rythampkhandelwal/WowSpotify/refs/heads/main/run.ps1'
-set url2='https://raw.githubusercontent.com/rythampkhandelwal/WowSpotify/refs/heads/main/run.ps1'
+set url='https://raw.githubusercontent.com/rythampkhandelwal/WowSpotify/refs/heads/master/run.ps1'
+set url2='https://raw.githubusercontent.com/rythampkhandelwal/WowSpotify/refs/heads/master/run.ps1'
 set tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
 
 %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe ^
