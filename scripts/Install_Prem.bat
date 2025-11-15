91fb433515ea48d83d51d0613057e5beb9c6693e
Rytham Khandelwal <rythampkhandelwal@gmail.com>
2025-11-15 18:02:46 +0530

    Update Install_Prem.bat

--- scripts/Install_Prem.bat
+++ scripts/Install_Prem.bat
@@ -1,10 +1,10 @@
 @echo off
 
-:: Line for changing spotx parameters, each parameter should be separated by a space
+:: Line for changing WowSpotify parameters, each parameter should be separated by a space
 set param=-premium -new_theme
 
-set url='https://raw.githubusercontent.com/rythampkhandelwal/SpotX/refs/heads/main/run.ps1'
-set url2='https://raw.githubusercontent.com/rythampkhandelwal/SpotX/refs/heads/main/run.ps1'
+set url='https://raw.githubusercontent.com/rythampkhandelwal/WowSpotify/refs/heads/master/run.ps1'
+set url2='https://raw.githubusercontent.com/rythampkhandelwal/WowSpotify/refs/heads/master/run.ps1'
 set tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
 
 %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe ^
